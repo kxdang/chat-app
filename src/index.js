b7156866983ad1e1a556f3640211b040c01c7305
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-11-04 15:34:57 -0500

    completing challenge

--- src/index.js
+++ src/index.js
@@ -12,17 +12,17 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
+// let count = 0;
 
 io.on("connection", socket => {
   console.log("New WebSocket connection");
-  socket.emit("countUpdated", count);
+  socket.emit("message", "Welcome!");
+  // socket.emit("countUpdated", count);
 
-  socket.on("increment", () => {
-    count++;
-    // socket.emit("countUpdated", count); //emits to single connection
-    io.emit("countUpdated", count); //emits to every single connection
-  });
+  // socket.on("increment", () => {
+  //   count++;
+  //   io.emit("countUpdated", count);
+  // });
 });
 
 server.listen(port, () => {
