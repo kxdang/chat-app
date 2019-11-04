3116688b128e6b646128977aab42ada35c7630b5
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-11-04 16:25:18 -0500

    adding socket event listeners to create a chat amongst clients in console

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,6 +3,14 @@ const socket = io();
 socket.on("message", message => {
   console.log(message);
 });
+
+document.querySelector("#message-form").addEventListener("submit", e => {
+  e.preventDefault();
+  const message = document.querySelector("input").value;
+  document.querySelector("input").value = "";
+  socket.emit("sendMessage", message);
+});
+
 // socket.on("countUpdated", count => {
 //   console.log("The count has been updated!", count);
 // });
