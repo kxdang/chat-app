28ca13534ce1cf530f55714f5a2d8f8024ed5758
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-11-04 12:30:53 -0500

    finalizing websocket connection with chatjs

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
+io.on("connection", () => {
+  console.log("New WebSocket connection");
+});
+
 server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
