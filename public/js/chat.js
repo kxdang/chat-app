813d603d43de535a8a2faaa5b090bbfafc426742
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-11-04 16:33:30 -0500

    refactoring the process of accessing the input element in case we encounter multiple input forms. Using the e.target method instead

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,16 +6,7 @@ socket.on("message", message => {
 
 document.querySelector("#message-form").addEventListener("submit", e => {
   e.preventDefault();
-  const message = document.querySelector("input").value;
-  document.querySelector("input").value = "";
+  const message = e.target.elements.message.value;
+  e.target.elements.message.value = "";
   socket.emit("sendMessage", message);
 });
-
-// socket.on("countUpdated", count => {
-//   console.log("The count has been updated!", count);
-// });
-
-// document.querySelector("#increment").addEventListener("click", () => {
-//   console.log("Clicked");
-//   socket.emit("increment");
-// });
