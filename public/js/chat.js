afb0112087a984421bd02fc5f8a0dff5a1ae988c
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-11-05 13:39:38 -0500

    using bad-words library to filter profanity

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,7 +8,14 @@ document.querySelector("#message-form").addEventListener("submit", e => {
   e.preventDefault();
   const message = e.target.elements.message.value;
   e.target.elements.message.value = "";
-  socket.emit("sendMessage", message);
+
+  socket.emit("sendMessage", message, error => {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Message delivered!");
+  });
 });
 
 document.querySelector("#send-location").addEventListener("click", () => {
