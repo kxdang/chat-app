673fdf02320d7bd4bf6c77de5352e2c72075ff94
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-11-08 15:53:53 -0500

    adding ability to track users joining and leaving

--- src/utils/users.js
+++ src/utils/users.js
@@ -43,3 +43,10 @@ const getUser = id => {
 const getUsersInRoom = room => {
   return users.filter(user => user.room === room.trim().toLowerCase());
 };
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+};
